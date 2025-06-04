c24b20f5ebd25cafe319ab3634eb5884b31fb37a
Diego Garcia <diegogarciatosta2804@gmail.com>
2025-06-04 20:09:11 -0300

    Update Home.jsx

--- projetogs/src/routes/Home.jsx
+++ projetogs/src/routes/Home.jsx
@@ -1,7 +1,7 @@
 function Home() {
   return (
     <div className="p-6 text-center">
-      <h1 className="text-3xl font-bold mb-4">Bem-vindo ao Sistema de Enchentes</h1>
+      <h1 className="text-3xl font-bold mb-4">Bem-vindo ao Sistema de Enchentes.</h1>
       <p className="text-gray-700">Navegue pelo menu acima para explorar as funcionalidades.</p>
     </div>
   )
