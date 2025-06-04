13799a67d561605c4e36889178816172d71c542e
Lucca Pereira Locambo De Oliveira <luccalocambopereira@gmail.com>
2025-06-04 20:12:25 -0300

    Update Error.jsx

--- projetogs/src/routes/Error.jsx
+++ projetogs/src/routes/Error.jsx
@@ -1,7 +1,7 @@
 function Error() {
   return (
     <div className="p-6 text-center">
-      <h1 className="text-3xl font-bold text-red-600 mb-4">Erro 404</h1>
+      <h1 className="text-3xl font-bold text-red-600 mb-4">Error 404</h1>
       <p className="text-gray-700">Página não encontrada.</p>
     </div>
   )
